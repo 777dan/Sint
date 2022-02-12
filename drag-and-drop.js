5300870c461b1be711499e1e3faf7987b07a235e
777dan <danya.nikitin7@gmail.com>
2022-02-12 11:55:22 +0200

    first commit

---
+++ drag-and-drop.js
@@ -0,0 +1,38 @@
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function drag(event) {
+    let sourse = event.target;
+    event.dataTransfer.setData("text", sourse.innerText);
+}
+
+function drop(event) {
+    event.preventDefault();
+    let data = event.dataTransfer.getData("text");
+    //  event.target.appendChild(document.getElementById(data));
+    let drop = event.target;
+    drop.innerHTML += data + " ";
+}
+
+let checkButton = document.getElementById("check");
+checkButton.addEventListener("click", check);
+
+function check(event) {
+    let div1 = document.getElementById("word");
+    let str = div1.innerText.trim();
+    let phrase = "cat and dog";
+    if (phrase == str) {
+        alert("!!!");
+    } else {
+        alert("???");
+    }
+}
+
+
+
+
+
+
+
+// var voiceSelect = document.getElementById("voice");
